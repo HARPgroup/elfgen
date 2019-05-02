4c6650a1e22d99cc5a3568da3b5cade04a29b1a1
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2019-05-02 10:30:48 -0400

    test-elfgen-getdata

--- tests/testthat/test-elfgen-getdata.R
+++ tests/testthat/test-elfgen-getdata.R
@@ -2,5 +2,11 @@ context("elfgen-getdata")
 
 test_that("Checking length of input watershed.code",
           {
-            expect_error(elfgen_getdata(9999999999999), "Invalid Length of Hydrologic Unit Code")
+            expect_error(elfgen_getdata("9999999999999"), "Invalid Length of Hydrologic Unit Code")
           })
+
+
+test_that("Function returns a dataframe", {
+  expect_equal(is.data.frame(elfgen_getdata("020700080403")), TRUE)
+})
+
