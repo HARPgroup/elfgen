8f5faedf35fe6297e95664f071c873761f18d4e1
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2019-06-26 08:56:20 -0400

    watershed name included on ELF plot

--- R/elfgen.R
+++ R/elfgen.R
@@ -75,7 +75,7 @@ elfgen <- function(watershed.df,quantile,breakpt,yaxis_thresh,xlabel = FALSE,yla
    }
 
    #Plot titles
-   plot_title <- paste("Watershed: ",watershed.df$watershede[1],"\n",sep="");
+   plot_title <- paste("Watershed: ",watershed.df$watershed[1],"\n",sep="");
    flow_title <- colnames(watershed.df.raw[1])
    biometric_title <- colnames(watershed.df.raw[2])
    if (xlabel != FALSE) {flow_title <- xlabel}
