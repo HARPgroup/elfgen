41f7fab6ad0387334fb3e845c9bcb1d63f65663a
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2019-08-26 13:29:26 -0400

    Improved error outputs

--- tests/testthat/test-bkpt-pwit.R
+++ tests/testthat/test-bkpt-pwit.R
@@ -12,7 +12,13 @@ test_that("Function returns a dataframe", {
   expect_equal(bkpt_pwit(test.watershed.df, 0.80, 50, 1000), 526)
   expect_equal(bkpt_pwit(test.watershed.df, 0.60, 50, 1000), 400)
   expect_equal(bkpt_pwit(test.watershed.df, 0.80, "bhi"= 1000), 526)
-  expect_equal(bkpt_pwit(test.watershed.df, 0.80, 600, 1000), "NONE IDENTIFIED")
+})
+
+test_that("Checking for No breakpoint identified", {
+  expect_error(bkpt_pwit("watershed.df" = test.watershed.df, 0.80, 600, 1000),
+               paste("No breakpoint identified using this set of inputs for ", test.watershed.df$watershed[1],
+                     "\n  ... Try using a smaller quantile or a wider bounding range",
+                     "\n  ... If still unsuccessful, a larger dataset may be required",sep=''))
 })
 
 test_that("Checking for quantile input parameter", {
