50de0ca16929242501b3f95973fc2096dafb5b26
Brogan <Connor.Brogan@deq.virginia.gov>
2026-09-15 10:26:44 -0400

    elfgen release to use hydrogeofetch

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' @format ## `watershed.df`
 #' A data frame with 81 rows and 15 columns:
 #' \describe{
-#'   \item{MAF}{Mean annual flow, as estimated from NHD estimation via \code{nhdPlusTools}}
+#'   \item{MAF}{Mean annual flow, as estimated from NHD estimation via \code{nhdPlusTools} (now \code{hydrogeofetch})}
 #'   \item{Q01,Q02, Q03, Q04, Q05, Q06, Q07, Q08, Q09, Q10, Q11, Q12}{Monthly estimated mean flow derived from NHD estimation via \code{nhdPlusTools}}
 #'   \item{NT.TOTAL.UNIQUE}{Total number of unique taxa}
 #'   \item{watershed.code}{Watershed NHD Code}
